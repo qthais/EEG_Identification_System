47852f240fb3db22e028abfea0e69941536436b1
qthais <thaiallb63@gmail.com>
2025-05-18 21:49:37 +0700

    feat: 93% 47k params

--- updateCnn2d.py
+++ updateCnn2d.py
@@ -16,7 +16,7 @@ SUBJECT_PREFIX = "S"
 EDF_KEYWORD = "R01"
 SAMPLE_RATE = 160  # EEG Sampling Rate
 TIME_WINDOW = 3  # 3 seconds per segment
-STRIDE = 0.5          # 1-second stride
+STRIDE = 0.3          # 1-second stride
 CHANNELS = ['Oz..', 'Iz..','P7..']  # 5 EEG Channels
 N_CLASSES = 109
 
