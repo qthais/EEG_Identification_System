3a6a051b99cc7370751e6dd4c56f1a05093daf07
qthais <thaiallb63@gmail.com>
2025-05-19 14:12:53 +0700

    feat(3s 93.4% 47k params): modify L2 to 0.02

--- updateCnn2d.py
+++ updateCnn2d.py
@@ -110,17 +110,17 @@ def build_cnn2d_model(input_shape, num_classes):
     inputs = Input(shape=input_shape)
 
     # CNN 2D layers
-    x = Conv2D(16, kernel_size=(3,3), activation='relu', padding='same', kernel_regularizer=tf.keras.regularizers.l2(0.05))(inputs)
+    x = Conv2D(16, kernel_size=(3,3), activation='relu', padding='same', kernel_regularizer=tf.keras.regularizers.l2(0.02))(inputs)
     x = BatchNormalization()(x)
     x = MaxPooling2D(pool_size=(2,2))(x)
-    x = Conv2D(32, kernel_size=(3,3), activation='relu', padding='same', kernel_regularizer=tf.keras.regularizers.l2(0.05))(x)
+    x = Conv2D(32, kernel_size=(3,3), activation='relu', padding='same', kernel_regularizer=tf.keras.regularizers.l2(0.02))(x)
     x = BatchNormalization()(x)
     x = MaxPooling2D(pool_size=(2,2))(x)
-    x = Conv2D(64, kernel_size=(3,3), activation='relu', padding='same', kernel_regularizer=tf.keras.regularizers.l2(0.05))(x)
+    x = Conv2D(64, kernel_size=(3,3), activation='relu', padding='same', kernel_regularizer=tf.keras.regularizers.l2(0.02))(x)
     x = BatchNormalization()(x)
     x = MaxPooling2D(pool_size=(2,2))(x)
     x = Flatten()(x)
-    x = Dense(64, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.05))(x)  # Thêm L2 regularization
+    x = Dense(64, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.02))(x)  # Thêm L2 regularization
     x= Dropout(0.4)(x)
     outputs = Dense(num_classes, activation='softmax')(x)
 
